a8c22df848ddf0ac78443c5178effcd961f05f4f
rachitGulati <rachitgulati26@gmail.com>
2023-09-09 18:46:20 +0530

    Third Commit: Added Server Endpoint to store subscription

--- app/index.js
+++ app/index.js
@@ -2,12 +2,26 @@ import express from 'express';
 import path from 'path';
 import bodyParser from 'body-parser';
 
+const LOCAL_STORAGE = {};
 const app = express();
 const port = 8080; // default port to listen
 
 // Serve all files in client
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(bodyParser.json());
+
+app.post('/subscription', async (req, res, next) => {
+  try {
+    const subscription = req.body;
+
+    LOCAL_STORAGE.subscription = subscription;
+
+    // Send 201 - resource created
+    res.status(201).json({ status: 'okay'});
+  } catch (e) {
+    next(e);
+  }
+});
 
 // start the Express server
 app.listen(port, () => {
